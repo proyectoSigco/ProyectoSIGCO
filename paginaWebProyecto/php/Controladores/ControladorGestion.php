3210b29c8cd093f68f389819346713cfc5f8eda3
Luis Carlos Diaz <lcdiaz3@misena.edu.co>
2015-08-12 01:45:07 -0500

    Formulario de gestion

---
+++ paginaWebProyecto/php/Controladores/ControladorGestion.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: probook
+ * Date: 12/08/15
+ * Time: 01:34 AM
+ */
---
+++ paginaWebProyecto/php/fachada/FacadeGestion.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: probook
+ * Date: 12/08/15
+ * Time: 01:42 AM
+ */
+
+interface FacadeGestion {
+
+}
